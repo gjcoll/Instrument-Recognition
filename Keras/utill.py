806a4827d43ef79aea436a6b7485d70063ea80d9
Anna Mookerjee <anm66@drexel.edu>
2019-03-05 20:21:13 -0500

    Update utill.py

--- Keras/utill.py
+++ Keras/utill.py
@@ -106,13 +106,16 @@ def spec_multiple(src_path, dest_path, done_folder, label, max_count = 2000):
 
     # Generate Mel spectrograms (128)
     melspecs_A = [mel_spec_it(x[0],x[1]) for x in A_norm]
+
+    #compress magnitudes with natural log
+    ln_melspecs_A = [np.log(abs(h) + np.finfo(np.float64).eps) for h in melspecs_A]
     # Generate Label Array based on input label
     # A_labels = np.full(shape = np.shape(melspecs_A)[0],fill_value = label)
     A_labels = np.tile(label,[np.shape(melspecs_A)[0],1])
     # Save to compressed file, file of two arrays 'data' = audio part (mel specs), 'labels' = label array
-    np.savez_compressed(dest_path, data = melspecs_A, labels = A_labels)
+    np.savez_compressed(dest_path, data = ln_melspecs_A, labels = A_labels)
 
-    return np.shape(melspecs_A)
+    return np.shape(ln_melspecs_A)
     
 
 
